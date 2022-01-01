2e5e4656bd101b78ce2e6f3869aae037293b17c6
praveen1437 <44938338+praveen1437@users.noreply.github.com>
2022-01-01 15:28:35 +0530

    String With unique characters

--- src/main/java/org/example/StringNoRepChar.java
+++ src/main/java/org/example/StringNoRepChar.java
@@ -1,4 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class StringNoRepChar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter string");
+        String s = sc.nextLine();
+        boolean isUnique = true;
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = 0; j < s.length(); j++){
+                if(i!=j) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        isUnique = false;
+                    }
+                }
+            }
+        }
+        if(isUnique) {
+            System.out.println("is unique");
+        } else {
+            System.out.println("not unique");
+        }
+    }
+
 }
